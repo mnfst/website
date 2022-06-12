95c1c5f7b6297a831f168b2ae7c8554692ab7530
Bruno <bruno@buddyweb.fr>
2022-06-12 18:16:54 +0200

    feat: basic contact form

--- src/partials/Footer.svelte
+++ src/partials/Footer.svelte
@@ -1,7 +1,40 @@
+<script lang="ts">
+	import { usageContents } from '../content/usages.content'
+	import type { Usage } from '../interfaces/usage.interface'
+
+	let usages: Usage[] = usageContents
+</script>
+
 <footer class="footer">
-	<div class="content has-text-centered">
-		<p>
-			<strong>CASE</strong>
-		</p>
+	<div class="columns">
+		<div class="column is-3">
+			<p><strong>CASE</strong></p>
+		</div>
+		<div class="column is-3">
+			<p><strong>Usages</strong></p>
+			<ul>
+				{#each usages as usage}
+					<li>
+						<a href="/usages/{usage.slug}">{usage.name}</a>
+					</li>
+				{/each}
+			</ul>
+		</div>
+		<div class="column is-3">
+			<p><strong>Communauté</strong></p>
+			<ul>
+				<li>Github</li>
+				<li>Discord</li>
+				<li>Stack overflow</li>
+			</ul>
+		</div>
+		<div class="column is-3">
+			<p><strong>Société</strong></p>
+			<ul>
+				<li>A propos</li>
+				<li>Carrières</li>
+				<li>Contact</li>
+			</ul>
+		</div>
 	</div>
 </footer>
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,1 +1,61 @@
-<h1 class="title is-1">Contact</h1>
+<script lang="ts">
+	const onSubmit = (event: any) => {
+		const formData: any = new FormData(event.target)
+
+		const data: any = {}
+		for (let field of formData) {
+			const [key, value] = field
+			data[key] = value
+		}
+		console.log(data)
+		// TODO: Send POST request to email API.
+	}
+</script>
+
+<div class="container">
+	<div class="columns">
+		<div class="column">
+			<h1 class="title is-1">Contact</h1>
+		</div>
+	</div>
+
+	<form on:submit|preventDefault={onSubmit}>
+		<div class="columns">
+			<div class="column">
+				<label for="name">Nom et prénom<span class="has-text-danger">*</span></label><br />
+				<input type="text" id="name" name="name" />
+			</div>
+		</div>
+		<div class="columns">
+			<div class="column">
+				<label for="company">Entreprise ou organisation<span class="has-text-danger">*</span></label
+				><br />
+				<input type="text" id="company" name="company" />
+			</div>
+		</div>
+		<div class="columns">
+			<div class="column">
+				<label for="email">Email<span class="has-text-danger">*</span></label><br />
+				<input type="email" id="email" name="email" />
+			</div>
+		</div>
+		<div class="columns">
+			<div class="column">
+				<label for="message">Message<span class="has-text-danger">*</span></label><br />
+				<textarea name="message" id="message" cols="30" rows="10" />
+			</div>
+		</div>
+		<div class="columns">
+			<div class="column">
+				<label for="attachment">Pièce jointe</label><br />
+				<input type="file" id="attachement" name="attachement" />
+			</div>
+		</div>
+
+		<div class="columns">
+			<div class="column">
+				<button class="button is-primary" type="submit">Envoyer</button>
+			</div>
+		</div>
+	</form>
+</div>
