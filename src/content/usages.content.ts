0d544999be44ce6a1a32c971a60c665aeb311163
Bruno <bruno@buddyweb.fr>
2022-06-15 09:52:35 +0200

    fix: usage page not working

--- src/content/usages.content.ts
+++ src/content/usages.content.ts
@@ -2,8 +2,18 @@ import type { Usage } from '../interfaces/usage.interface'
 
 export const usageContents: Usage[] = [
 	{
+		name: 'Opérationnel',
+		slug: 'operationnel',
+		image: 'operations.jpg',
+		description: 'Gestion de projet, travaux, livrables et recette',
+		title: 'Opérationnel',
+		subtitle:
+			'Sur le terrain, en chaîne de production ou au bureau, chacun collabore avec CASE en partageant son activité en temps réel.'
+	},
+	{
 		name: 'Planification',
 		slug: 'planification',
+		image: 'operations.jpg',
 		description: 'Organisation des agendas, des équipes et du temps passé',
 		title:
 			'Une app de planification pour gérer les ressources humaines et physiques de votre activité',
@@ -13,6 +23,7 @@ export const usageContents: Usage[] = [
 	{
 		name: 'Reporting et analytique',
 		slug: 'reporting-et-analytique',
+		image: 'operations.jpg',
 		description: 'Graphiques, tableaux, rapports, etc.',
 		title:
 			'Une app de planification pour gérer les ressources humaines et physiques de votre activité',
@@ -22,6 +33,7 @@ export const usageContents: Usage[] = [
 	{
 		name: 'Devis et factures',
 		slug: 'devis -et-factures',
+		image: 'operations.jpg',
 		description: 'Devis et factures',
 		title:
 			'Une app de planification pour gérer les ressources humaines et physiques de votre activité',
