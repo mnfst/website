ad8a59d82aba11dbe1a89aad48b8cb8573adb098
Bruno <bruno@buddyweb.fr>
2023-09-20 16:55:37 +0200

    tests: add more e2e tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -3,4 +3,21 @@ describe('CASE Website', () => {
     cy.visit('/')
     cy.contains('CRUD')
   })
+
+  it('has a link to the GitHub repo', () => {
+    cy.visit('/')
+    cy.contains('GitHub').should('have.attr', 'href').and('include', 'github')
+  })
+
+  it('has a link to the documentation', () => {
+    cy.visit('/')
+    cy.contains('Get started')
+      .should('have.attr', 'href')
+      .and('include', 'https://docs.case.app')
+  })
+
+  it('has a FAQ page', () => {
+    cy.visit('/faq')
+    cy.contains('FAQ')
+  })
 })
