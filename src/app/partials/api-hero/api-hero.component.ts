755a9718fa2e01affacec324783f90fd0739502b
Sébastien Conejo <sebastien.conejo@buddyweb.fr>
2023-11-03 16:26:33 +0100

    style: finish to style api panel including  on mobile and tablet

--- src/app/partials/api-hero/api-hero.component.ts
+++ src/app/partials/api-hero/api-hero.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-api-hero',
   templateUrl: './api-hero.component.html',
-  styleUrls: ['./api-hero.component.css']
+  styleUrls: ['./api-hero.component.scss']
 })
 export class ApiHeroComponent {
 
