63e298bf929a0bb57523dde8640a9a44b2c96784
Bruno <bruno@buddyweb.fr>
2024-03-11 20:42:00 +0100

    replace Angular by NextJS

--- src/app/pages/integration/integration.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { IntegrationComponent } from './integration.component'
-
-describe('IntegrationComponent', () => {
-  it('should mount', () => {
-    cy.mount(IntegrationComponent)
-  })
-})
--- src/app/partials/sdk-features/sdk-features.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { SdkFeaturesComponent } from './sdk-features.component'
-
-describe('SdkFeaturesComponent', () => {
-  it('should mount', () => {
-    cy.mount(SdkFeaturesComponent)
-  })
-})
