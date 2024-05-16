063b03ad19bf477b43050b396729b6036d7c21e3
Bruno <bruno@buddyweb.fr>
2024-05-16 14:58:48 +0200

    feat(docs): sidebar

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -62,7 +62,7 @@ const config: Config = {
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          sidebarId: 'docSidebar',
           position: 'left',
           label: 'Tutorial'
         },
