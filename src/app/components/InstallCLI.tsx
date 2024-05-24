a9cd0885a65ab549012d16ad68602b4e0c76f857
Bruno <bruno@buddyweb.fr>
2024-05-24 17:00:37 +0200

    home: copy script to clipboard

--- src/app/components/InstallCLI.tsx
+++ src/app/components/InstallCLI.tsx
@@ -1,41 +1,38 @@
-import React from 'react';
+'use client'
+
+import React, { useState } from 'react'
 
 const InstallCLI: React.FC = () => {
-    return (
-        <div className="is-flex is-justify-content-center">
-            <div
-                className="is-flex is-flex-direction-column is-align-items-flex-start is-justify-content-flex-start npx-wrapper"
-            >
-                <div
-                    className="has-border-radius is-flex is-full-height isalign-items-stretch is-family-code p-0 has-background-dark has-text-light has-shadow-success"
-                >
-                    <span className="px-4 py-2 install-code">
-                        npx create-case-app my-case-app
-                    </span>
-                    <a
+  const [isCopied, setIsCopied] = useState(false)
+  const command = 'npx add-manifest'
 
-                        className="icon-text has-background-grey-darker px-4 py-2 is-flex is-align-items-center has-text-white is-relative"
-                    >
-                        <span className="icon">
-                            <i className="icon icon-copy"></i>
-                        </span>
-                        <span
-                            className="is-inline-flex is-flex-direction-column is-relative is-hidden-touch has-text-centered"
-                        >
-                            <span
-                                className="button-copy"
+  const handleCopy = async () => {
+    await navigator.clipboard.writeText(command)
+    setIsCopied(true)
+  }
 
-                            >Copy</span
-                            >
-                            <span className="is-copied"
-                            >Copied !</span
-                            >
-                        </span>
-                    </a>
-                </div >
-            </div >
-        </div >
-    )
+  return (
+    <div className="is-flex is-justify-content-center">
+      <div className="is-flex is-flex-direction-column is-align-items-flex-start is-justify-content-flex-start npx-wrapper">
+        <div className="has-border-radius is-flex is-full-height is-align-items-stretch is-family-code p-0 has-background-dark has-text-light has-shadow-success">
+          <span className="px-4 py-2 install-code">{command}</span>
+          <a
+            className="icon-text has-background-grey-darker px-4 py-2 is-flex is-align-items-center has-text-white is-relative"
+            onClick={handleCopy}
+          >
+            <span className="icon">
+              <i className="icon icon-copy"></i>
+            </span>
+            <span className="is-inline-flex is-flex-direction-column is-relative is-hidden-touch has-text-centered">
+              <span className="button-copy">
+                {isCopied ? 'Copied !' : 'Copy'}
+              </span>
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default InstallCLI
