5e799354e9019eb81bd7f9141447d399f0ff905b
Sébastien Conejo <sebastien.conejo@buddyweb.fr>
2024-05-24 17:08:25 +0200

    style: add stylized code box

--- src/app/components/LiveCodeHero.tsx
+++ src/app/components/LiveCodeHero.tsx
@@ -3,15 +3,12 @@ import React from 'react';
 const LiveCodeHero: React.FC = () => {
     return (
         <div className="box-wrapper">
-            <div
-                className="box box--code is-flex is-align-items-flex-start is-justify-content-flex-start is-flex-direction-column px-0 pt-0 pb-3 is-shadowless"
-            >
-                <img
-                    src="./assets/images/yaml-file.svg"
-                    alt="example of yaml file"
-                    loading="lazy"
-                />
-            </div>
+            <img
+                src="./assets/images/yaml-file.svg"
+                alt="example of yaml file"
+                loading="lazy"
+                className="yaml-code is-fullwidth"
+            />
         </div>
     )
 }
