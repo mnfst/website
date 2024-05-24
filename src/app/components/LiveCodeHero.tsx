66316145f8d860d56fd6c0ee5a264e7ef9c1ec49
Bruno <bruno@buddyweb.fr>
2024-05-24 16:52:39 +0200

    style: SCSS styles on separate file

--- src/app/components/LiveCodeHero.tsx
+++ src/app/components/LiveCodeHero.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
+import React from 'react'
+import './LiveCodeHero.scss'
 
-const LiveCodeHero: React.FC = () => {
-    return (
-        <div className="box-wrapper">
-            <div
-                className="box box--code is-flex is-align-items-flex-start is-justify-content-flex-start is-flex-direction-column px-0 pt-0 pb-3 is-shadowless"
-            >
-                <img
-                    src="./assets/images/yaml-file.svg"
-                    alt="example of yaml file"
-                    loading="lazy"
-                />
-            </div>
-        </div>
-    )
-}
+const LiveCodeHero: React.FC = () => (
+  <div className="box-wrapper">
+    <div className="box box--code is-flex is-align-items-flex-start is-justify-content-flex-start is-flex-direction-column px-0 pt-0 pb-3 is-shadowless">
+      <img
+        src="./assets/images/yaml-file.svg"
+        alt="example of yaml file"
+        loading="lazy"
+      />
+    </div>
+  </div>
+)
 
 export default LiveCodeHero
