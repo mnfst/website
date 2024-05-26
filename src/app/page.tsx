2486ef229ac98688bd1c5f0431350cb99d20f01a
Sébastien Conejo <sebastien.conejo@buddyweb.fr>
2024-05-26 23:04:09 +0200

    style: add how it works section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main>
       <div>
-        <div className="container is-fluid has-background-glass">
+        <div className="has-background-glass">
           <div className="container is-header">
             <nav
               className="navbar has-background-transparent"
@@ -61,18 +61,19 @@ export default function Home() {
             </nav>
           </div>
         </div>
-        <div >
-          <div className="container ">
+        <div className="my-6">
+
+          <div className="container">
             <div className="columns is-multiline">
               <div className="column is-6">
-                <div className="content has-background-warning-light ">
-                  <h1 className="title is-3 mb-5">
+                <div className="content  ">
+                  <h1 className="title is-1 mb-5">
                     Building{' '}
                     <span className="title-gradient">the simplest backend</span>{' '}
                     in the world
                   </h1>
 
-                  <p className="content has-background-warning-light  is-normal">
+                  <p className="content   is-normal">
                     Manifest is the simplest Backend-as-a-Service for rapid web
                     application development
                   </p>
@@ -80,8 +81,100 @@ export default function Home() {
                 </div>
               </div>
               <div className="column is-6-tablet is-5-desktop is-offset-0-tablet is-offset-1-desktop">
-                <div className="content has-background-warning-light ">
+                <div className="content  ">
                   <LiveCodeHero />
+                </div>
+              </div>
+              <div className="column is-12">
+                <div className="card has-background-glass">
+                  <div className="card-content has-text-centered card-content--how-it-works">
+                    <div className="content">
+                      <h2 className="title is-2">Define your data structure,<br />get a backend ready for your business application.</h2>
+                      <div className="backend-list is-boredered is-rounded is-flex is-align-items-center is-justify-content-center my-6">
+                        <span className="is-flex is-align-items-center is-justify-content-center is-flex-direction-row backend-list__item">
+                          <span className="icon  backend-list__icon">
+                            <i className="lni lni-checkmark has-text-success"></i>
+                          </span>
+                          <span>Database</span>
+                        </span>
+                        <span className="is-flex is-align-items-center is-justify-content-center is-flex-direction-row backend-list__item">
+                          <span className="icon  backend-list__icon">
+                            <i className="lni lni-checkmark has-text-success"></i>
+                          </span>
+                          <span>API</span>
+                        </span>
+                        <span className="is-flex is-align-items-center is-justify-content-center is-flex-direction-row backend-list__item">
+                          <span className="icon  backend-list__icon">
+                            <i className="lni lni-checkmark has-text-success"></i>
+                          </span>
+                          <span>Rest API</span>
+                        </span>
+                        <span className="is-flex is-align-items-center is-justify-content-center is-flex-direction-row backend-list__item">
+                          <span className="icon  backend-list__icon">
+                            <i className="lni lni-checkmark has-text-success"></i>
+                          </span>
+                          <span>Admin panel</span>
+                        </span>
+
+                      </div>
+
+                      <div className="stack-list is-flex is-align-items-center is-justify-content-center is-flex-wrap-wrap">
+                        <span>It works with your favorite weapon:</span>
+                        <span
+                          title="See how to plug CASE with React"
+                        >
+                          <img
+                            src="./assets/images/react.svg"
+                            alt="React logo"
+                            width="40px"
+                            className="mx-2"
+                          />
+                        </span>
+                        <span
+                          title="See how to plug CASE with Vue"
+                        >
+                          <img
+                            src="./assets/images/vue.svg"
+                            alt="Vue logo"
+                            width="40px"
+                            className="mx-2"
+                          />
+                        </span>
+                        <span
+                          title="See how to plug CASE with Angular"
+                        >
+                          <img
+                            src="./assets/images/angular.svg"
+                            alt="Angular logo"
+                            width="40px"
+                            className="mx-2"
+                          />
+                        </span>
+                        <span
+                          title="See how to plug CASE with Svelte"
+                        >
+                          <img
+                            src="./assets/images/svelte.svg"
+                            alt="Svelte logo"
+                            width="40px"
+                            className="mx-2"
+                          />
+                        </span>
+                        <img
+                          src="./assets/images/nuxt.svg"
+                          alt="Nuxt logo"
+                          width="40px"
+                          className="mx-2"
+                        />
+                        <img
+                          src="./assets/images/nextjs.svg"
+                          alt="NextJS logo"
+                          width="70px"
+                          className="mx-2"
+                        />
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
               <div className="column is-12">
@@ -92,6 +185,6 @@ export default function Home() {
         </div>
 
       </div>
-    </main>
+    </main >
   )
 }
