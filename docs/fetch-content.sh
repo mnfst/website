34589a59383cbde94171805ec5af93fa4ae803e9
Bruno <bruno@buddyweb.fr>
2024-09-11 16:28:43 +0200

    feat(dev): update fetch content script for current tested branch

--- docs/fetch-content.sh
+++ docs/fetch-content.sh
@@ -3,4 +3,4 @@
 rm -rf content
 
 # Clone the repository into the 'content' directory
-git clone --depth 1 --branch master https://github.com/mnfst/docs.git content
+git clone --depth 1 --branch feat/validation https://github.com/mnfst/docs.git content
