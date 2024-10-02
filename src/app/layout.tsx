3d097bf3f5fe4f226dddc9f65e6646d71cb0ac6a
Sébastien Conejo <sebastien@buddyweb.fr>
2024-10-02 18:03:40 +0200

    feat: add snippet to have visibility into the organizations that are viewing the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+import { GoogleAnalytics } from '@next/third-parties/google'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import HotjarSnippet from './components/Hotjar'
-import { GoogleAnalytics } from '@next/third-parties/google'
 import './globals.scss'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -36,6 +37,28 @@ export default function RootLayout({
         {children}
         <HotjarSnippet />
         <GoogleAnalytics gaId="G-FPJ74C43L9" />
+        <Script id="cr-relay-script" strategy="afterInteractive">
+          {`
+            (function() {
+              if (typeof window === 'undefined') return;
+              if (typeof window.signals !== 'undefined') return;
+              var script = document.createElement('script');
+              script.src = 'https://cdn.cr-relay.com/v1/site/43b5a956-ea4c-403e-8f29-fb180fc604a9/signals.js';
+              script.async = true;
+              window.signals = Object.assign(
+                [],
+                ['page', 'identify', 'form'].reduce(function (acc, method){
+                  acc[method] = function () {
+                    signals.push([method, arguments]);
+                    return signals;
+                  };
+                return acc;
+                }, {})
+              );
+              document.head.appendChild(script);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   )
