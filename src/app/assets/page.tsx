0cd9b13b979a2a593a6c6f32326b3c8f32067779
Sébastien Conejo <sebastien@buddyweb.fr>
2024-10-22 14:32:51 +0200

    feat: add logomarks

--- src/app/assets/page.tsx
+++ src/app/assets/page.tsx
@@ -51,7 +51,7 @@ export default function PressAssetsPage() {
                         <img
                           src="./assets/images/logotype-white.svg"
                           alt="Dark Logo Manifest"
-                          className="logotype-presentation"
+                          className="logo-presentation"
                         />
 
                         <p className="buttons download-buttons">
@@ -85,7 +85,7 @@ export default function PressAssetsPage() {
                         <img
                           src="./assets/images/logotype-dark.svg"
                           alt="Dark Logo Manifest"
-                          className="logotype-presentation"
+                          className="logo-presentation"
                         />
                         <p className="buttons download-buttons">
                           <a
@@ -122,9 +122,173 @@ export default function PressAssetsPage() {
                 <div className="card has-background-white is-shadowless">
                   <div className="is-flex is-align-items-center is-justify-content-center is-fullheight logotype-wrapper is-relative">
                     <img
-                      src="./assets/images/co-brand.svg"
+                      src="./assets/images/logotype/co-brand.svg"
                       alt="Dark Logo Manifest"
-                      className="logotype-presentation"
+                      className="logo-presentation"
+                    />
+                  </div>
+                </div>
+
+                <h2>Logomark</h2>
+                <p>
+                  For optimal brand recognition, we recommend using the full
+                  Manifest logotype. The logomark can be used in situations
+                  where space is limited and displaying the full logotype isn't
+                  possible.
+                </p>
+                <div className="columns is-multiline">
+                  {/* Transparent Logo */}
+                  <div className="column is-6">
+                    <div className="card has-background-white-bis  is-shadowless">
+                      <div className="is-flex is-align-items-center is-justify-content-center is-fullheight logotype-wrapper is-relative">
+                        <img
+                          src="./assets/images/logomark/logo.svg"
+                          alt="Logomark Manifest"
+                          className="logo-presentation"
+                        />
+
+                        <p className="buttons download-buttons">
+                          <a
+                            className="button is-small is-outlined is-dark"
+                            download
+                            href="/assets/images/logomark/logo.svg"
+                          >
+                            <span className="icon is-small">
+                              <i className="lni lni-download"></i>
+                            </span>
+                            <span>SVG</span>
+                          </a>
+                          <a
+                            className="button is-small is-outlined is-dark"
+                            download
+                            href="/assets/images/logomark/logo.png"
+                          >
+                            <span className="icon is-small">
+                              <i className="lni lni-download"></i>
+                            </span>
+                            <span>PNG</span>
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  {/* Dark Logo */}
+                  <div className="column is-6">
+                    <div className="card has-background-dark is-shadowless">
+                      <div className="is-flex is-align-items-center is-justify-content-center is-fullheight logotype-wrapper is-relative">
+                        <img
+                          src="./assets/images/logomark/logo.svg"
+                          alt="Dark Logo Manifest"
+                          className="logo-presentation"
+                        />
+                        <p className="buttons download-buttons">
+                          <a
+                            className="button is-small is-outlined is-white"
+                            download
+                            href="/assets/images/logomark/logo-dark.svg"
+                          >
+                            <span className="icon is-small">
+                              <i className="lni lni-download"></i>
+                            </span>
+                            <span>SVG</span>
+                          </a>
+                          <a
+                            className="button is-small is-outlined is-white"
+                            download
+                            href="/assets/images/logomark/logo-dark.png"
+                          >
+                            <span className="icon is-small">
+                              <i className="lni lni-download"></i>
+                            </span>
+                            <span>PNG</span>
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* Sand Logo */}
+                  <div className="column is-6">
+                    <div className="card has-background-warning-light is-shadowless">
+                      <div className="is-flex is-align-items-center is-justify-content-center is-fullheight logotype-wrapper is-relative">
+                        <img
+                          src="./assets/images/logomark/logo.svg"
+                          alt="Dark Logo Manifest"
+                          className="logo-presentation"
+                        />
+                        <p className="buttons download-buttons">
+                          <a
+                            className="button is-small is-outlined is-dark"
+                            download
+                            href="/assets/images/logomark/logo-sand.svg"
+                          >
+                            <span className="icon is-small">
+                              <i className="lni lni-download"></i>
+                            </span>
+                            <span>SVG</span>
+                          </a>
+                          <a
+                            className="button is-small is-outlined is-dark"
+                            download
+                            href="/assets/images/logomark/logo-sand.png"
+                          >
+                            <span className="icon is-small">
+                              <i className="lni lni-download"></i>
+                            </span>
+                            <span>PNG</span>
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* White Logo */}
+                  <div className="column is-6">
+                    <div className="card has-background-white is-shadowless">
+                      <div className="is-flex is-align-items-center is-justify-content-center is-fullheight logotype-wrapper is-relative">
+                        <img
+                          src="./assets/images/logomark/logo.svg"
+                          alt="Dark Logo Manifest"
+                          className="logo-presentation"
+                        />
+                        <p className="buttons download-buttons">
+                          <a
+                            className="button is-small is-outlined is-dark"
+                            download
+                            href="/assets/images/logomark/logo-white.svg"
+                          >
+                            <span className="icon is-small">
+                              <i className="lni lni-download"></i>
+                            </span>
+                            <span>SVG</span>
+                          </a>
+                          <a
+                            className="button is-small is-outlined is-dark"
+                            download
+                            href="/assets/images/logomark/logo-white.png"
+                          >
+                            <span className="icon is-small">
+                              <i className="lni lni-download"></i>
+                            </span>
+                            <span>PNG</span>
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <h2>Co-branding logomark</h2>
+                <p>
+                  The spacing around the Manifest logo is determined by its
+                  design. Unless specified by partner brands, all logos should
+                  be treated as a balanced collection of shapes.
+                </p>
+                <div className="card has-background-white is-shadowless">
+                  <div className="is-flex is-align-items-center is-justify-content-center is-fullheight logotype-wrapper is-relative">
+                    <img
+                      src="./assets/images/logomark/co-brand.svg"
+                      alt="Dark Logo Manifest"
+                      className="logo-presentation logo-presentation--small"
                     />
                   </div>
                 </div>
