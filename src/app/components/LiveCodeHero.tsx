941d6fb6d02ce160fe78538ae46ddbd88005a6b1
Bruno <bruno@buddyweb.fr>
2024-11-25 15:33:13 +0100

    feat(editor): tabs

--- src/app/components/LiveCodeHero.tsx
+++ src/app/components/LiveCodeHero.tsx
@@ -1,36 +1,65 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import './LiveCodeHero.scss'
 
 const LiveCodeHero: React.FC = () => {
-    return (
-        <div className="box-wrapper has-background-glass has-border-gradient">
-            <span className="cc-dark"><i className="lni lni-remove-file "></i> backend.yml</span>
-            <pre className="mt-4">
-                <code className="javascript">
-                    <span className="cn ci0"><span className="cc-green">name</span><span className="cc-dark">: </span>Healthcare application</span><br /><br />
-                    <span className=""><span className="cc-green">entities</span><span className="cc-dark">: </span></span><br />
-                    <span className="ci1"><span className="cc-green">Doctor</span><span className="cc-dark">: </span></span><br />
-                        <span className="ci2"><span className="cc-green">properties</span><span className="cc-dark">: </span></span><br />
-                            <span className="ci3"><span className='cc-dark'>- </span>fullName</span><br />
-                            <span className="ci3"><span className='cc-dark'>- </span>avatar</span><br />
-                            <span className="ci3"><span className='cc-dark'>- </span><span className="cc-sand">{`{`}</span> <span className="cc-green">name</span><span className="cc-dark">: </span>price,  <span className="cc-green">type</span><span className="cc-dark">: </span>money,  <span className="cc-green">options</span><span className="cc-dark">: </span><span className="cc-sand">{`{`}</span> <span className="cc-green">currency</span><span className="cc-dark">: </span>EUR <span className="cc-sand">{`}`} </span><span className="cc-sand">{`}`}</span></span><br />
-                        <span className="ci2"><span className="cc-green">belongsTo</span><span className="cc-dark">: </span></span><br />
-                            <span className="ci3"><span className='cc-dark'>- </span>City</span><br />
-                        <br />
-                    <span className="ci1"><span className="cc-green">Patient</span><span className="cc-dark">: </span></span><br />
-                        <span className="ci2"><span className="cc-green">properties</span><span className="cc-dark">: </span></span><br />
-                        <span className="ci3"><span className='cc-dark'>- </span>fullName</span><br />
-                        <span className="ci3"><span className='cc-dark'>- </span><span className="cc-sand">{`{`}</span> <span className="cc-green">name</span><span className="cc-dark">: </span>birthdate,  <span className="cc-green">type</span><span className="cc-dark">: </span>date <span className="cc-sand">{`}`}</span></span><br />
-                        <span className="ci2"><span className="cc-green">belongsTo</span><span className="cc-dark">: </span></span><br />
-                        <span className="ci3"><span className='cc-dark'>- </span>Doctor</span><br />
-                        <br />
-                    <span className="ci1"><span className="cc-green">City</span><span className="cc-dark">: </span></span><br />
-                        <span className="ci2"><span className="cc-green">properties</span><span className="cc-dark">: </span></span><br />
-                        <span className="ci2"><span className='cc-dark'>- </span>name</span><br />
-                </code>
-            </pre>
-        </div>
-    )
+  const [activeTab, setActiveTab] = useState(0)
+
+  const tabs = [
+    {
+      label: 'Data models',
+      content: 'test 1'
+    },
+    {
+      label: 'Auth',
+      content: 'test 2'
+    },
+    {
+      label: 'Validation',
+      content: 'test 3'
+    },
+    {
+      label: 'Storage',
+      content: 'test 4'
+    }
+  ]
+
+  return (
+    <div>
+      {/* Tab Headers */}
+      <div style={{ display: 'flex', borderBottom: '2px solid #ccc' }}>
+        {tabs.map((tab, index) => (
+          <button
+            key={index}
+            onClick={() => setActiveTab(index)}
+            style={{
+              padding: '10px 20px',
+              cursor: 'pointer',
+              border: 'none',
+              borderBottom: activeTab === index ? '3px solid #0070f3' : 'none',
+              backgroundColor: 'transparent',
+              fontWeight: activeTab === index ? 'bold' : 'normal'
+            }}
+          >
+            {tab.label}
+          </button>
+        ))}
+      </div>
+
+      {/* Tab Content: editor */}
+      <div
+        style={{
+          height: 640,
+          width: 1136,
+          backgroundColor: 'black',
+          color: 'white'
+        }}
+      >
+        {tabs[activeTab].content}
+      </div>
+    </div>
+  )
 }
 
 export default LiveCodeHero
