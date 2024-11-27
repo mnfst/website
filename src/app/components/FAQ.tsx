32bfba6cdee5591e3e5e75520b828c45af38768e
Sébastien Conejo <sebastien@buddyweb.fr>
2024-11-27 17:28:52 +0100

    feat: add animation to the faq

--- src/app/components/FAQ.tsx
+++ src/app/components/FAQ.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './FAQ.scss'
 
 type FAQItem = {
@@ -10,38 +10,48 @@ type FAQItem = {
 
 export const FAQ: React.FC = () => {
   const [openIndex, setOpenIndex] = useState<number | null>(null)
+  const contentRefs = useRef<Array<HTMLDivElement | null>>([])
 
   const faqs: FAQItem[] = [
     {
-      question: 'Is Manifest a framework ?',
+      question: 'Is Manifest a framework?',
       answer:
         'No, Manifest is a self-hostable Backend-as-a-Service (BaaS). It is a service that enables developers to easily build a backend, providing the essential building blocks to get started.'
     },
     {
-      question: 'Is Manifest full-stack ?',
+      question: 'Is Manifest full-stack?',
       answer:
         'No, you will still need to develop your frontend. Manifest covers the backend of your app/website only (considering the built-in admin panel as part of the backend/back-office).'
     },
     {
-      question: 'Is Manifest for me ?',
+      question: 'Is Manifest for me?',
       answer:
         'If you know a bit of software development, whatever your programming language is, you will have no problem using Manifest.'
     },
     {
-      question: 'Can I use Manifest in production ?',
+      question: 'Can I use Manifest in production?',
       answer:
         'Manifest is currently in BETA, you can use it at your own risk. It is stable enough to power small projects, prototypes and MVPs but we do not recommend to use it on critical platforms.'
     },
     {
-      question: 'What is behind the magic ?',
+      question: 'What is behind the magic?',
       answer:
-        'Good sense and common top stacks: the current version runs on NodeJS, Express and NestJS. The Database is SQLite, using TypeORM and JS-YAML to go convert your YAML into a DB structure '
+        'Good sense and common top stacks: the current version runs on NodeJS, Express and NestJS. The Database is SQLite, using TypeORM and JS-YAML to go convert your YAML into a DB structure.'
     }
   ]
 
   const toggleFAQ = (index: number) => {
     setOpenIndex(index === openIndex ? null : index)
   }
+
+  useEffect(() => {
+    contentRefs.current.forEach((content, index) => {
+      if (content) {
+        content.style.height =
+          openIndex === index ? `${content.scrollHeight}px` : '0px'
+      }
+    })
+  }, [openIndex])
 
   return (
     <div className="content">
@@ -66,11 +76,17 @@ export const FAQ: React.FC = () => {
               }`}
             ></i>
           </a>
-          {openIndex === index && (
-            <div id={`faq-content-${index}`} className="faq-answer">
-              <p>{faq.answer}</p>
-            </div>
-          )}
+          <div
+            id={`faq-content-${index}`}
+            ref={(el) => (contentRefs.current[index] = el)}
+            className="faq-answer"
+            style={{
+              overflow: 'hidden',
+              transition: 'height 0.3s ease-in-out'
+            }}
+          >
+            <p>{faq.answer}</p>
+          </div>
         </div>
       ))}
     </div>
