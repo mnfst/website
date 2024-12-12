43573f8caef8c16fbc2217758b4074a070fbb527
Sébastien Conejo <sebastien@buddyweb.fr>
2024-12-12 18:45:06 +0100

    feat: add cookieYes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,25 @@
-import type { Metadata } from 'next'
+import type { Metadata as NextMetadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import Script from 'next/script'
 import React from 'react'
+import HotjarSnippet from './components/Hotjar'
 import Footer from './components/layout/Footer'
 import Header from './components/layout/Header'
 import './globals.scss'
 
 const inter = Inter({ subsets: ['latin'] })
+
+interface Metadata extends NextMetadata {
+  openGraph: {
+    url: string
+    title: string
+    description: string
+    siteName: string
+    type: string
+    images: Array<{ url: string; type: string }>
+  }
+}
 
 export const metadata: Metadata = {
   title: 'Manifest - The 1-file Headless CMS',
@@ -20,7 +34,8 @@ export const metadata: Metadata = {
     siteName: 'Manifest',
     images: [
       {
-        url: 'https://manifest.build/assets/images/og-image.png'
+        url: 'https://manifest.build/assets/images/og-image.png',
+        type: 'image/png'
       }
     ]
   }
@@ -32,6 +47,46 @@ const RootLayout: React.FC<{
   return (
     <html lang="en" data-theme="light" className="has-navbar-fixed-top">
       <body className={inter.className}>
+        <Head>
+          <title>{String(metadata.title) ?? 'Default Title'}</title>
+          <meta
+            name="description"
+            content={metadata.description ?? 'Default description'}
+          />
+          <meta property="og:type" content={metadata.openGraph?.type} />
+          <meta
+            property="og:url"
+            content={metadata.openGraph?.url?.toString()}
+          />
+          <meta
+            property="og:title"
+            content={String(metadata.openGraph?.title)}
+          />
+          <meta
+            property="og:description"
+            content={metadata.description ?? undefined}
+          />
+          <meta
+            property="og:site_name"
+            content={metadata.openGraph?.siteName}
+          />
+          <meta
+            property="og:image"
+            content={
+              Array.isArray(metadata.openGraph?.images) &&
+              metadata.openGraph.images[0] &&
+              'url' in metadata.openGraph.images[0]
+                ? metadata.openGraph.images[0].url.toString()
+                : ''
+            }
+          />
+          <HotjarSnippet />
+          <Script
+            id="cookieyes"
+            type="text/javascript"
+            src="https://cdn-cookieyes.com/client_data/d8a8f8d74309d7ede1458cdf/script.js"
+          ></Script>
+        </Head>
         <Header />
         <main>{children}</main>
         <Footer />
