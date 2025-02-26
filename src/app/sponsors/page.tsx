12600e2b817f370a313725172ed3a24842e64ed0
Sébastien Conejo <sebastien@buddyweb.fr>
2025-02-26 17:44:14 +0100

    fix: remove french comments

---
+++ src/app/sponsors/page.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Sponsors - Manifest',
+  description: 'Become an Official Manifest Sponsor'
+}
+
+export default function PressAssetsPage() {
+  return (
+    <div>
+      <div className="mt-6 pt-4 mb-6">
+        <div className="container zi-1">
+          <div className="columns is-multiline">
+            <div className="column is-8 is-offset-2">
+              <div className="content">
+                <h1>Brand assets</h1>
+              </div>
+            </div>
+            <div className="column is-8 is-offset-2">
+              <div className="content">
+                <p>Hola</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
