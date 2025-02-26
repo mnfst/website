04913cc56068d947320995314a6c1af7e5256596
Sébastien Conejo <sebastien@buddyweb.fr>
2025-02-26 18:08:49 +0100

    feat: start adding content to sponsors page

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -13,12 +13,119 @@ export default function PressAssetsPage() {
           <div className="columns is-multiline">
             <div className="column is-8 is-offset-2">
               <div className="content">
-                <h1>Brand assets</h1>
+                <h1>Become an offical Manifest Sponsor</h1>
               </div>
             </div>
             <div className="column is-8 is-offset-2">
               <div className="content">
-                <p>Hola</p>
+                <p>
+                  Support the development of Manifest and help shape the future
+                  of web development.
+                </p>
+                <p>
+                  The easiest way to partner with us is through{' '}
+                  <strong>sponsorship</strong>. Your contributions fund the
+                  ongoing maintenance and growth of this{' '}
+                  <strong>open-source project</strong>.
+                </p>
+                <p>
+                  Sponsors at certain tiers receive logo placement and links on
+                  key Manifest platforms like our homepage, GitHub README, and
+                  documentation site.
+                </p>
+                <p>
+                  Become a sponsor: Join via{' '}
+                  <a
+                    href="https://opencollective.com/mnfst"
+                    target="_blank"
+                    className=""
+                  >
+                    Open Collective
+                  </a>{' '}
+                  or GitHub Sponsors (Coming soon).
+                </p>
+
+                <p>Thank you for supporting Manifest!</p>
+                <div className="card is-bordered is-shadowless is-dark">
+                  <div className="card-content">
+                    <div className="content">
+                      <p>
+                        Need a <strong>custom sponsorship tier</strong>? Contact
+                        us at
+                        <a
+                          className="ml-1"
+                          href="mailto:partner@manifest.build"
+                          target="_blank"
+                        >
+                          partner@manifest.build
+                        </a>
+                        .
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <h2>Why Sponsor Manifest?</h2>
+                <div className="columns">
+                  <div className="column">
+                    <div className="card is-shadowless">
+                      <div className="card-content">
+                        <div className="content">
+                          <h3>For businesses 📈</h3>
+                          <ul>
+                            <li>
+                              <strong>Ensure long-term sustainability</strong>{' '}
+                              of a solution you rely on
+                            </li>
+                            <li>
+                              <strong>Gain visibility</strong> in the Manifest
+                              ecosystem
+                            </li>
+                            <li>
+                              Support feature development that benefits your
+                              business
+                            </li>
+                            <li>
+                              Demonstrate commitment to{' '}
+                              <strong>open source</strong>
+                            </li>
+                            <li>
+                              Potential <strong>tax benefits</strong> for
+                              supporting an open-source project
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="column">
+                    <div className="card is-shadowless is-fullheight">
+                      <div className="card-content">
+                        <div className="content">
+                          <h3>For individuals 🧑‍💻</h3>
+                          <ul>
+                            <li>
+                              <strong>Support a tool you love</strong> and use
+                              daily
+                            </li>
+                            <li>
+                              Help maintain and improve independent{' '}
+                              <strong>open-source development</strong>
+                            </li>
+                            <li>
+                              <strong>Get recognition</strong> for your
+                              contribution
+                            </li>
+                            <li>
+                              Keep Manifest{' '}
+                              <strong>free and open-source for everyone</strong>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
