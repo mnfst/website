9821186406514e4b3689fde7972ae446b470e14a
Bruno <bruno@buddyweb.fr>
2025-06-17 15:44:46 +0200

    feat(analytics): install vercel analytics

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,12 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser'
+import { appConfig } from './app/app.config'
+import { AppComponent } from './app/app.component'
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { inject } from '@vercel/analytics'
+import { environment } from './environments/environment'
+
+inject({
+  mode: environment.production ? 'production' : 'development'
+})
+
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err))
