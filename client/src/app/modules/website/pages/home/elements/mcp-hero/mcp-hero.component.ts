9ee16faeb058828b01737d15421130c97cb3b9f8
Sébastien Conejo <sebastien@buddyweb.fr>
2025-11-25 17:13:52 -0800

    feat: add diagram on home page

--- client/src/app/modules/website/pages/home/elements/mcp-hero/mcp-hero.component.ts
+++ client/src/app/modules/website/pages/home/elements/mcp-hero/mcp-hero.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core'
 import { ClientLogosComponent } from '../../../../../../common/partials/client-logos/client-logos.component'
 import { WaitingListFormComponent } from '../../../../../../common/partials/waiting-list-form/waiting-list-form.component'
+import { DiagramComponent } from './diagram/diagram.component'
 
 @Component({
   selector: 'app-mcp-hero',
   standalone: true,
-  imports: [ClientLogosComponent, WaitingListFormComponent],
+  imports: [ClientLogosComponent, WaitingListFormComponent, DiagramComponent],
   templateUrl: './mcp-hero.component.html',
   styleUrls: ['./mcp-hero.component.scss']
 })
