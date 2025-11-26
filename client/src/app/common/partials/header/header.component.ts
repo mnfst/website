976fb9b428f21272cbe87e917fac3d0e214e5f7f
Sébastien Conejo <sebastien@buddyweb.fr>
2025-11-25 17:53:03 -0800

    feat: add github logo on the heaer

--- client/src/app/common/partials/header/header.component.ts
+++ client/src/app/common/partials/header/header.component.ts
@@ -1,15 +1,48 @@
 // src/app/common/partials/header/header.component.ts
-import { Component, Renderer2 } from '@angular/core'
+import { Component, Renderer2, OnInit, Inject, PLATFORM_ID } from '@angular/core'
+import { isPlatformBrowser } from '@angular/common'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   private static scriptInjected = false
+  githubStars = 'Star'
 
-  constructor(private renderer: Renderer2) {}
+  constructor(
+    private renderer: Renderer2,
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.fetchGitHubStars()
+    }
+  }
+
+  private fetchGitHubStars(): void {
+    this.http.get<any>('https://api.github.com/repos/mnfst/manifest')
+      .subscribe({
+        next: (response) => {
+          const stars = response.stargazers_count
+          this.githubStars = `${this.formatStarCount(stars)}`
+        },
+        error: () => {
+          this.githubStars = 'Star'
+        }
+      })
+  }
+
+  private formatStarCount(count: number): string {
+    if (count >= 1000) {
+      return `${(count / 1000).toFixed(1)}k`
+    }
+    return count.toString()
+  }
 
   onGetDemo(): void {
     window.open('https://calendly.com/sebastien-manifest/30min', '_blank')
